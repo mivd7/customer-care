fb8b7c85b78ac690e9b06a6d1ce60ff8d59eafd3
mivd7 <mi.vandeurzen@gmail.com>
2019-11-06 10:52:12 +0100

    send mail request

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,12 +2,12 @@
     <div>
         <div v-if="!showNextStep" class="cc-form">
             <div class="cc-form__header"><b>Welcome!<br/></b><img src="../assets/cc-icon.png" alt="cc-icon" class="cc-form__icon"/></div>
-            <p>Please fill out your complete details in order to continue.</p>
+            <p>Please fill out your complete details to continue.</p>
             <div class="cc-form__fields">
                 <form @submit="handleSubmit">
                     <input placeholder="First Name" v-model="firstName"/>
                     <input placeholder="Last Name" v-model="lastName"/>
-                    <input placeholder="E-mail" v-model="formModel.EMAIL"/>
+                    <input placeholder="E-mail" v-model="formModel.email"/>
                     <input placeholder="Registered Phone No." v-model="formModel.phonenumber"/><br/>
                     <a @click="goToNextStep"><div class="cc-form__btn">Next</div></a>
                 </form>
@@ -15,13 +15,13 @@
         </div>
         <div v-else class="cc-form">
             <div class="cc-form__header">
-                <b>What's up, {{firstName}}!?<br/></b><img src="../assets/cc-icon.png" alt="cc-icon" class="cc-form__icon"/>
+                <b>What's up, {{firstName}}?<br/></b><img src="../assets/cc-icon.png" alt="cc-icon" class="cc-form__icon"/>
             </div>
             <form class="cc-form__fields">
                 <br/>
                 <textarea name="message" rows="50" cols="30" v-model="formModel.request" placeholder="Tell us..."></textarea>
                 <br>
-                <span><label>Unsubscribe from our services?</label><input type="checkbox" id="checkbox" v-model="unsubscribe"><br/></span>
+                <span><label>Unsubscribe from our services?</label><input type="checkbox" id="checkbox" v-model="formModel.unsubscribe"><br/></span>
                 <a @click="handleSubmit"><div class="cc-form__btn">Submit</div></a>
             </form>
         </div>
@@ -33,18 +33,20 @@
 </template>
     
 <script>
+import axios from 'axios';
+import {baseUrl} from '../constants';
 
 export default {
     name: 'Form',
     data() {
       return {
         formModel: {
-          EMAIL: '',
+          email: '',
           fullname: '',
           phonenumber: '',
           request: '',
+          unsubscribe: false,
         },
-        unsubscribe: false,
         firstName: '',
         lastName: '',
         showNextStep: false,
@@ -53,21 +55,28 @@ export default {
     },
     methods: {
         goToNextStep() {
-            const {firstName, lastName} = this;
+            const { firstName, lastName } = this;
             if (firstName !== '' && lastName !== '') {
                 this.formModel.fullname = this.formModel.fullname.concat(firstName, ' ', lastName);
-                this.showNextStep = true
-            } 
+            } else {
+                this.formModel.fullname = firstName
+            }
+            this.showNextStep = true
         },
         handleSubmit() {
-            const {EMAIL, fullname, phonenumber, request} = this.formModel;
-            if(EMAIL === '' || fullname === '' || phonenumber === '' || request === '') {
+            const { email, fullname, phonenumber, request } = this.formModel;
+            if(email === '' || fullname === '' || phonenumber === '' || request === '') {
                 console.warn('not all fields completed!', this.formModel)
             } else {
                 this.isDone = true
-                console.log('submit!')
-                //api call
+                this.sendMessage(this.formModel)
             }
+        },
+        async sendMessage(messageRequest) {
+            console.log('sending:', messageRequest)
+            await axios.post(`${baseUrl}/sendMail/`, messageRequest)
+                       .then(response => console.log('message send!', response.data))
+                       .catch(err => console.error('Error sending message!', err))
         },
         reset() {
             location.reload();
@@ -122,13 +131,18 @@ export default {
 		height: 42px;
 		margin: 5px 0;
 		border: none;
-		border-radius: 30px;
+        border-radius: 30px;
         background-color: whitesmoke;
-        padding: 10px;
+        padding: 12.5px;
         padding-left: 25px;
-        font-size: 1.4rem;
+        font-size: 1.2rem;
         font-weight: 100;
         text-align: left;
+    }
+
+    .cc-form__fields input::placeholder {
+        color: grey;
+        font-size: 1rem;
     }
 
     .cc-form__fields textarea {
